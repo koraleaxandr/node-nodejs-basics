685415ff760ab7858633a65ba8a89de51c7239a8
Alexandr Karatkevich <87863536+koraleaxandr@users.noreply.github.com>
2022-06-03 22:47:39 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,21 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import {
+    fileURLToPath
+} from 'url';
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    const originFolderPath = path.join(__dirname, 'files', 'fileToRead.txt');
+    try {
+     const file =  await fs.readFile(originFolderPath, {encoding: 'utf-8' });
+     console.log(file);
+    } catch (error) {
+     console.log('FS operation failed');
+    }
 };
+
+read();
