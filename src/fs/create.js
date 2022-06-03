a436375d524f36548b5a8b40432fdafcc98c1d3e
koraleaxandr <koraleaxandr@gmail.com>
2022-06-03 09:55:56 +0300

    Feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,12 +1,18 @@
-import * as fs from "fs";
+import * as fs from 'fs';
 import * as path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathString = path.join(__dirname, 'files', 'fresh.txt');
 
 export const create = async () => {
-     fs.writeFile(
-    path.join(path.dirname, 'files', 'fresh.txt'),
-    'I am fresh and young', {flag: 'ax'}
-    (err) => {
-        if (err) throw err;
+    try {
+        await fs.writeFile(pathString,
+            'I am fresh and young', {flag: 'ax'});
+            console.log('1');
+    } catch (error) {        
         console.log('FS operation failed');        
-    });
+    };
 };
+
+create();
