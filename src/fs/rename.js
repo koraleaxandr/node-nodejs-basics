37088e89c74977f914c1f00ae829aca0ff89d96c
Alexandr Karatkevich <87863536+koraleaxandr@users.noreply.github.com>
2022-06-03 22:20:25 +0300

    fix: edit rename error message

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -7,7 +7,12 @@ import {
 export const rename = async () => {
     const __filename = fileURLToPath(
         import.meta.url);
-    const __dirname = path.dirname(__filename)
+    const __dirname = path.dirname(__filename);
+    try {
+        await fs.realpath(`${__dirname}/files/properFilename.md`)
+        console.log('FS operation failed');
+        return;
+    } catch (error) {}
     try {
         await fs.rename(`${__dirname}/files/wrongFilename.txt`, `${__dirname}/files/properFilename.md`)
     } catch (error) {
