ac4c4152b911bddbf3fdccb478c212e255c29003
koraleaxandr <koraleaxandr@gmail.com>
2022-06-03 12:40:22 +0300

    fix: temp save

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,24 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import {
+    fileURLToPath
+} from 'url';
+
 export const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(
+        import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const originFolderPath = path.join(__dirname, 'files');    
+    console.log(originFolderPath);
+    const targetFolderPath = path.join(__dirname, 'files_copy');
+    try {
+        const files = await fs.readdir(originFolderPath, {withFileTypes : true});
+        console.log(files);
+        for (const file of files)
+          console.log(file);
+      } catch (err) {
+        console.log('FS operation failed');
+      }
 };
+
+copy();
