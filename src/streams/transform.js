3e4d3a2eb078bb5c456a4bb89d8ca6e854c7b5b3
koraleaxandr <koraleaxandr@gmail.com>
2022-06-06 14:37:56 +0300

    feat: implement transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,32 @@
+import { stdin, stdout } from 'process';
+import { pipeline, Transform } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+    console.log('ENTER DATA OR  Ctrl + C for exit');
+    const input = stdin;
+    const output = stdout;
+    const decode = new Transform({
+        transform(chunk, encoding, callback) {
+            let newChunk = '';
+            chunk = chunk.toString(); 
+              for (let i = chunk.length-1; i>= 0; i = i - 1) {
+                  newChunk += chunk[i];
+              }
+              chunk = newChunk + `\n`;
+          callback(null, chunk);
+        },
+      });
+
+    pipeline(
+        input,
+        decode,
+        output,
+        err => {
+            if (err) {
+                console.log('transform operation failed');
+            }
+        }
+    );
 };
+
+transform();
