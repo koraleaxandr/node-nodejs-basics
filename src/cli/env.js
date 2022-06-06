b215cc4e7b7f28927ca8ef046d0cc7dc46163a76
koraleaxandr <koraleaxandr@gmail.com>
2022-06-06 10:08:54 +0300

    feat: implement hash

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,4 @@
 export const parseEnv = () => {
-    // const variables = Object.entries(process.env);
-    // variables.forEach(element => {
-    //     console.log(`RSS_${element.key} = ${element.value}`);        
-    // });
     for (let key in process.env) {
         console.log(`RSS_${key} = ${key.value}`);        
        };    
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,28 @@
+const {
+    createHash
+  } = await import('crypto');
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import {
+    fileURLToPath
+} from 'url';
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const file =  await fs.open(`${__dirname}/files/fileToCalculateHashFor.txt`);
+    const input = file.createReadStream();
+    input.on('readable', () => {
+        const data = input.read();
+        if (data)
+          hash.update(data);
+        else {
+          console.log(`${hash.digest('hex')}`);
+        }
+      });
 };
+
+calculateHash();
