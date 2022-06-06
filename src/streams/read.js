44e59b2a22a663c2b000f5ec73b114c6cd5d09bc
koraleaxandr <koraleaxandr@gmail.com>
2022-06-06 13:26:01 +0300

    feat: implement read/ write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,21 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { stdout } from 'process';
+import {
+    fileURLToPath
+} from 'url';
+
 export const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(
+        import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const inputFilePath = `${__dirname}/files/fileToRead.txt`;
+    const input = await fs.readFile(inputFilePath, err => {
+        if (err) {
+            console.log('zip operation failed');
+        }
+    });
+    stdout.write(input);
 };
+
+read();
