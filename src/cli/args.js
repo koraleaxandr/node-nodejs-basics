49e1964e632f8348e61bfaf9ba15bb85ba943e33
koraleaxandr <koraleaxandr@gmail.com>
2022-06-06 08:37:58 +0300

    feat: implement cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.forEach((element, index) => {
+        if (index % 2 === 0) {
+            console.log(`RSS_${element} = ${args[index +1]}`);
+        } 
+    });
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    // const variables = Object.entries(process.env);
+    // variables.forEach(element => {
+    //     console.log(`RSS_${element.key} = ${element.value}`);        
+    // });
+    for (let key in process.env) {
+        console.log(`RSS_${key} = ${key.value}`);        
+       };    
 };
+
+parseEnv();
