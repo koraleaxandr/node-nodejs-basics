e328e6fe603c5cbbf0160da530d49f8fc48df3d2
koraleaxandr <koraleaxandr@gmail.com>
2022-06-07 10:23:22 +0300

    fix: add cli tesing comment

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,10 +1,17 @@
+import { stdout } from 'process';
+
 export const parseArgs = () => {
     const args = process.argv.slice(2);
+    let argsString = '';
     args.forEach((element, index) => {
         if (index % 2 === 0) {
-            console.log(`RSS_${element} = ${args[index +1]}`);
+            let endOfString = index >= args.length-2 ? '':', ';
+            argsString+= `RSS_${element} = ${args[index +1] + endOfString}`;
         } 
     });
+    console.log(argsString);
 };
 
 parseArgs();
+
+//for testing - npm run src/cli/args 11 22 33 44 55 ...and so on
